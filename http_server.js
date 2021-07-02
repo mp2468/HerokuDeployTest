4aeff8e1c9f439ddf70a7a676d06f56b5b73e81d
mp2468 <pianomanmark@gmail.com>
2021-07-02 16:32:23 -0400

    updating cors policy

--- http_server.js
+++ http_server.js
@@ -6,8 +6,16 @@ const adapter = new fs('db.json');
 const db = low(adapter);
 const cors = require('cors');
 
+var corsOptions = {
+    origin: '*',
+    methods: 'GET,PUT,POST',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Range'],
+    exposedHeaders: ['Content-Range', 'X-Content-Range', 'Accept-Ranges', 'Content-Encoding', 'Content-Length'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 // allow cross-origin resource sharing (CORS)
-app.use(cors());
+app.use(cors(corsOptions));
 
 // data parser - used to parse post data
 var bodyParser = require('body-parser');
